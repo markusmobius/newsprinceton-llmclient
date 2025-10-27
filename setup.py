835449ecbbd0c9b8ac3527100d5caf00d7ebd46b
Markus Mobius <markusmobius@gmail.com>
2025-10-27 03:00:13 -0400

    last updates

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='LlmClient',
-    version='1.0.3',
+    version='1.0.4',
     description='Python library for LLM client',
     url='https://github.com/markusmobius/newsprinceton-llmclient',
     packages=find_packages(),
     install_requires=[
         'jsonschema',
         'aiohttp',
-        'asyncio'
+        'asyncio',
+        'requests'
     ],    
     include_package_data=True
 )
