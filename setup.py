70a8825f3534b7762a032699bcec3115ae9994bc
Markus Mobius <mobius@microsoft.com>
2025-11-19 01:52:24 -0500

    Fixes

--- setup.py
+++ setup.py
@@ -2,14 +2,15 @@ from setuptools import setup, find_packages
 
 setup(
     name='LlmClient',
-    version='1.0.7',
+    version='1.0.8',
     description='Python library for LLM client',
     url='https://github.com/markusmobius/newsprinceton-llmclient',
     packages=find_packages(),
     install_requires=[
         'jsonschema',
         'grpcio',
-        'requests'
+        'requests',
+        'protobuf'
     ],    
     include_package_data=True
 )
