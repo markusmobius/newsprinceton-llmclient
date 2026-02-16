a0a3525cc1565ab410fddb6b29597cd43b7e36c8
Markus Mobius <mobius@microsoft.com>
2026-02-16 03:53:26 -0500

    Fixes

--- test_image.py
+++ test_image.py
@@ -1,7 +1,7 @@
 import asyncio
 import urllib.request
 from LlmClient.LlmLib import LlmFactory
-from LlmClient.Models import Chat, MessageFragment
+from LlmClient.Models import Chat, MessageFragments
 
 async def process_chat(chat : Chat, client):
     output=await client.Ask(chat,tags=["example"])
@@ -49,11 +49,11 @@ descriptions=[]
 # Chat 1: Is this a cat? (With cat image)
 cat_url = "https://www.llmserver.econlabs.org/assets/cat.png"
 cat_image_bytes = get_image_bytes(cat_url)
-fragment_cat = MessageFragment()
+fragment_cat = MessageFragments()
 fragment_cat.AddText("Is this a cat?")
 fragment_cat.AddImage(cat_image_bytes, "image/png")
 chat_cat = Chat(responseSchema=yesno_schema)
-chat_cat.AddSystemMessage("You are a helpful assistant capable of image recognition.")
+chat_cat.AddSystemMessage("You are a helpful assistant capable of  image recognition.")
 chat_cat.AddUserMessageList(fragment_cat)
 chats.append(chat_cat)
 descriptions.append(f"Is there a cat in this cat image: {cat_url}")
@@ -61,11 +61,11 @@ descriptions.append(f"Is there a cat in this cat image: {cat_url}")
 # Chat 2: Is this a cat? (With not-a-cat image)
 cat_url = "https://www.llmserver.econlabs.org/assets/notacat.png"
 cat_image_bytes = get_image_bytes(cat_url)
-fragment_cat = MessageFragment()
+fragment_cat = MessageFragments()
 fragment_cat.AddText("Is this a cat?")
 fragment_cat.AddImage(cat_image_bytes, "image/png")
 chat_cat = Chat(responseSchema=yesno_schema)
-chat_cat.AddSystemMessage("You are a helpful assistant capable of image recognition.")
+chat_cat.AddSystemMessage("You are a helpful assistant capable of  image recognition.")
 chat_cat.AddUserMessageList(fragment_cat)
 chats.append(chat_cat)
 descriptions.append(f"Is there a cat in this not-a-cat image: {cat_url}")
