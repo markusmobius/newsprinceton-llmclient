59956ab2d84158c96f0638a1777000d300712cf6
Markus Mobius <mobius@microsoft.com>
2026-02-17 04:15:03 -0500

    Fixes

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='LlmClient',
-    version='1.0.31',
+    version='1.0.32',
     description='Python library for LLM client',
     url='https://github.com/markusmobius/newsprinceton-llmclient',
     packages=find_packages(),
@@ -11,7 +11,8 @@ setup(
         'grpcio',
         'requests',
         'protobuf',
-        'python-magic'
+        'python-magic-bin; sys_platform == "win32"',
+        'python-magic; sys_platform != "win32"',
     ],    
     include_package_data=True
 )
